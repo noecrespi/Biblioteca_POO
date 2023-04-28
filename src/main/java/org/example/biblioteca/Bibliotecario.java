22bb8793ecfcb94d1f23becacc08606e1448da86
Noelia <ncrespipomar@cifpfbmoll.eu>
2023-04-28 13:11:43 +0200

    add class Bibliotecario

--- src/main/java/org/example/biblioteca/Bibliotecario.java
+++ src/main/java/org/example/biblioteca/Bibliotecario.java
@@ -1,4 +1,10 @@
 package org.example.biblioteca;
 
-public class Bibliotecario {
+public class Bibliotecario extends Persona{
+    //Desarrolla la clase Bibliotecario. Heredará los atributos anteriores, pero además deseamos almacenar su puesto de
+    // trabajo, NIF y contraseña. Crea constructor vacío, todos los parámetros (incluidos los de la superclase), copia,
+    // getters/setters y toString (NO imprimirá los datos de la superclase).
+
+    String puestoTrabajo;
+    String NIF;
 }
